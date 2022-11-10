6f561de3e5e6077dd2a65dbcadb4b9672eb6a81e
Brian Smawley <briansmawley@gmail.com>
2022-11-10 15:41:25 -0500

    Redux additions to main branch

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,20 @@
+// src/components/visibility-filter-input/visibility-filter-input.jsx
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Form from 'react-bootstrap/Form';
+
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+  return <Form.Control
+    onChange={e => props.setFilter(e.target.value)}
+    value={props.visibilityFilter}
+    placeholder="filter"
+  />;
+}
+
+export default connect(
+  null,
+  { setFilter }
+)(VisibilityFilterInput);
